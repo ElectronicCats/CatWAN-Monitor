ad59bb7598c9d96e6a8969103fb8a46d4a736bc2
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-12-09 14:08:05 -0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1000000,
+              fallback: "file-loader",
+              name: "images/[name].[hash].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
