12a04220fa3f14e352c2c62908c548fed3593d54
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-12-09 14:14:50 -0600

    delete fragment component react

--- app/renderer/app.js
+++ app/renderer/app.js
@@ -1,12 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-
 /**Components */
 import MainComponent from "./components/MainComponent";
-import Navbar from "./components/Navbar";
 
 const rootElement = document.querySelector(
   document.currentScript.getAttribute("data-container")
@@ -15,11 +13,8 @@ const rootElement = document.querySelector(
 // Render the main component, the parent for everything else on the page
 // Wrap the app and allow redux store access
 ReactDOM.render(
-  <Fragment>
-    <Navbar />
-    <Provider store={store}>
-      <MainComponent />
-    </Provider>
-  </Fragment>,
+  <Provider store={store}>
+    <MainComponent />
+  </Provider>,
   rootElement
 );
