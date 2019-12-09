087ff0114107ac513a343684867763868dab6d58
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-12-09 14:17:02 -0600

    remove actions

--- app/renderer/actions/action-types.js
+++ app/renderer/actions/action-types.js
@@ -1,18 +1,11 @@
 /* Action types
     - simply used to keep action strings referenced to a name ... more reliable? ... if you can't remember a string I guess
     */
-export const SEND_COMMAND = "SEND_COMMAND";
-export const SENT_COMMAND = "SENT_COMMAND";
-
-/* Admin */
-export const SWITCH_MOCK_OBC = "SWITCH_MOCK_OBC"
-
-/* Epoch */
-export const INCREMENT_EPOCH = "INCREMENT_EPOCH";
-
 /**
  * Serial port types
  */
 
 export const GET_DATA_PORTS = "GET_PORTS";
 export const SET_SERIAL_PORTS = "SET_SERIAL_PORTS";
+export const SEND_COMMAND = "SEND_COMMAND";
+export const SENT_COMMAND = "SENT_COMMAND";
